bcbf8a7bd06693c31090306cbbdd8c31815333ca
Chris Sammy <sammy.chrisammy.chris30@gmail.com>
2022-12-13 19:30:35 +0000

    lowercase alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,9 +4,9 @@
  * print_alphabet - print all alphabet in lowercase
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
-	char leeter;
+	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
