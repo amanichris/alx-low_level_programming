fa13f04c8f180ab702fd7b31d2f2432ae8d29b3d
Chris Sammy <sammy.chrisammy.chris30@gmail.com>
2022-12-14 04:51:27 +0000

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
- * _isalpha - checks for char alphabetical letters
+ * _isalpha - checks for char alphabetical letters.
  * @c: the char to be checked
  * Return: returns 0 or 1 depending on condition
  */
+
 int _isalpha(int c)
 {
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && <= 'Z'));
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
-#ifndef MAIN_H                                                                
-#define MAIN_H                                                               
-int _putchar(char c);                                                         void print_alphabet(void); 
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
@@ -10,5 +11,5 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to_98(int n);                                                        
-#endif 
+void print_to_98(int n);
+#endif
