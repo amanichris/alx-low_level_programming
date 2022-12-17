8b7dbdb2210c5901d269c2f0b8e5ff1645304e7f
Chris Sammy <sammy.chrisammy.chris30@gmail.com>
2022-12-17 02:29:19 +0000

    print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,13 +7,13 @@
 
 void print_square(int size)
 {
-	int x, y;
+	int X, y;
 
 	if (size <= 0)
 		_putchar('\n')
 	else
 	{
-		for (x = 0; x < size; x++)
+		for (X = 0; X < size; x++)
 		{
 			for (y = 0; y < size; y++)
 		{
