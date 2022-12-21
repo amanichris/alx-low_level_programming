086a7e3ce01dc4a56f3e9fe73f0a4c9957e3d5fa
Chris Sammy <sammy.chrisammy.chris30@gmail.com>
2022-12-21 21:29:49 +0000

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strncpy -> for copying purpose
+* @dest: param1
+* @src: param2
+* @n: param
+* Return: string
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
