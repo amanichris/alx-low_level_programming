b317929563f77f87a8b1868c942cd60f1daa49a9
Chris Sammy <sammy.chrisammy.chris30@gmail.com>
2022-12-21 22:24:29 +0000

    string to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* string_toupper -> converting any string to upper case
+* @x: string param
+* Return: string
+*/
+char *string_toupper(char *)
+{
+	int i = 0;
+
+	while (x[i])
+	{
+	if (x[i] >= 97 && x[i] <= 122)
+		x[i] = x[i] - 32;
+	i++;
+	}
+	return (x);
+}
