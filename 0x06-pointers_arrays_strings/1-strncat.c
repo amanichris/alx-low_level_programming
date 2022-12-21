0d8a0cd449f5430ae6710b01a1cea3ec73d25280
Chris Sammy <sammy.chrisammy.chris30@gmail.com>
2022-12-21 21:11:29 +0000

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* *_strncat -> function to append some char
+* @dest: first param
+* @src: second param
+* @n: 3rd param
+* Return: string
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(*dest, *src, n);
+	return (dest);
+}
