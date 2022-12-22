16d7e537887e1ac47bca07657d44d042e9499e15
Chris Sammy <sammy.chrisammy.chris30@gmail.com>
2022-12-22 16:06:02 +0000

    Print integer number

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,10 +9,23 @@
 char *rot13(char *)
 {
 	int indx1 = 0, indx2;
-	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J','K', 'L', 'M', 'N', 'O' 'P', 'Q', 'R', 'S' 'T', 'U' 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
-	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
-'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'm', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',	
+			'G', 'H', 'I', 'J', 'K', 'L',
+			'M', 'N', 'O', 'P', 'Q', 'R',               
+			'S', 'T', 'U', 'V', 'W', 'X',							'Y', 'Z', 'a', 'b', 'c', 'd',
+				'e', 'f', 'g', 'h', 'i', 'j',
+				'k', 'l', 'm', 'n', 'o', 'p',
+				'q', 'r', 's', 't', 'u', 'v',
+				'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 
+			     'T', 'U', 'V', 'W', 'X', 'Y', 
+                             'Z', 'A', 'B', 'C', 'D', 'E', 
+                             'F', 'G', 'H', 'I', 'J', 'K', 
+                             'L', 'M', 'm', 'o', 'p', 'q', 
+                             'r', 's', 't', 'u', 'v', 'w', 
+                             'x', 'y', 'z', 'a', 'b', 'c', 
+                             'd', 'e', 'f', 'g', 'h', 'i', 
+                             'j', 'k', 'l', 'm'};
 	while (str[indx1])
 	{
 		for (indx2 = 0; indx2 < 52; indx2++)
@@ -27,4 +40,3 @@ char *rot13(char *)
 	}
 	return (str);
 }
-	
