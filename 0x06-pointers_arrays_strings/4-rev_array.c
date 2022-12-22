61aaacaeec41d3ff6354e5ae8af0bead148c71d3
Chris Sammy <sammy.chrisammy.chris30@gmail.com>
2022-12-22 00:24:17 +0000

    reversing array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,16 +8,13 @@
 */
 void reverse_array(int *a, int n)
 {
-	int *p, i, aux, k;
+	int i, j;
 
-	p = a;
 	for (i = 0; i < n; i++)
-		p++;
-	for (k = 0; k < n / 2; k++)
 	{
-		aux = a[k];
-		a[k] = *p
-		* p = aux;
-		p--;
+		n--;
+		j = a[i];
+		a[i] = a[n];
+		a[n] = j;
 	}
 }
