258e3ba2ffc75bfdf940d2e2b4d54424ace0ca8a
Chris Sammy <sammy.chrisammy.chris30@gmail.com>
2023-01-02 12:55:52 +0000

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -13,11 +13,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned char index;
-	unsigned char *memory = s, value = b;
+	int i = 0;
 
-	for (index = 0; index < n; index++)
-		memory[index] = value;
-
-	return (memory);
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
 }
