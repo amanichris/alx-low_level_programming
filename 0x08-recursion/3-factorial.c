32cdb0319f3a2918d37d03d78864bcf91e571ba6
Chris Sammy <sammy.chrisammy.chris30@gmail.com>
2023-01-04 14:01:47 -0800

    task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,11 +10,12 @@
 
 int factorial(int n)
 {
-	int result = n;
+	if (n == 0)
+		return (1);
 
-	if (n < 0)
+	else if (n < 0)
 		return (-1);
 
-	else if (n >= 0 && n <= 1)
-		return (0);
+	else
+	return (n * factorial(n - 1));
 }
