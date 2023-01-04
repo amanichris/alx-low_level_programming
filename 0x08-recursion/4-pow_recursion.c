9d16ccfd6c579529d448fdf8e4cedae9276184d0
Chris Sammy <sammy.chrisammy.chris30@gmail.com>
2023-01-04 14:25:37 -0800

    pow file

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,11 +9,14 @@
  */
 int _pow_recursion(int x, int y)
 {
-	int result = x;
-
 	if (y < 0)
-		return (-1);
+	{
+	return (-1);
+	}
 
-	else if (y == 0)
-		return (1);
+	else if (y != 0)
+		return (x * _pow_recursion(x, y - 1));
+
+	else
+	return (1);
 }
