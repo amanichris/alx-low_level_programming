a5a2df82da212168aeb2522b577ed61bd50c27c0
Chris Sammy <sammy.chrisammy.chris30@gmail.com>
2023-01-16 16:44:33 -0800

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,51 +2,88 @@
 #include "dog.h"
 
 /**
- * new_dog - creates a new dog
- * @name: First member
- * @age: Second member
- * @owner: Third member
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
  *
- *  Return: NULL if fail
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * _strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the buffer pointed to by dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: pointer to the new dog (Success), NULL otherwise
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int i, 1enN, 1eno;
+	dog_t *dog;
+	int len1, len2;
 
-	struct dog *n_dog = NULL;
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
 
-	1enN = 0;
-	while (name[1enN] != '\0')
-		1enN++;
-	1eno = 0;
-	while (owner[1eno] != '\0')
-		1eno++;
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
 
-	n_dog = malloc(sizeof(struct dog));
-	if (n_dog == NULL)
+	dog->name = malloc(sizeof(char) * (len1 + 1));
+	if (dog->name == NULL)
 	{
-		free(n_dog);
+		free(dog);
 		return (NULL);
 	}
-	n_dog->name = malloc(1enN + 1);
-	if (n_dog->name == NULL)
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
+	if (dog->owner == NULL)
 	{
-		free(n_dog->name);
-		free(n_dog);
+		free(dog);
+		free(dog->name);
 		return (NULL);
 	}
-	n_dog->owner = malloc(1eno + 1);
-	if (n_dog->owner == NULL)
-	{
-		free(n_dog->name);
-		free(n_dog->owner);
-		free(n_dog);
-		return (NULL);
-	}
-	for (i = 0; i <= 1enN; i++)
-		n_dog->name[i] = name[i];
-	for (i = 0; i <= 1eno; i++)
-		n_dog->owner[i] = owner[i];
-	n_dog->age = age;
-	return (n_dog);
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	dog->age = age;
+
+	return (dog);
 }
