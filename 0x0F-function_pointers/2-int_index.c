90284e0dad599602858313a5187c6cb8e2047c72
Chris Sammy <sammy.chrisammy.chris30@gmail.com>
2023-01-18 14:53:54 -0800

    task int index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -18,8 +18,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		for (i = 0; i < size; i++)
 		{
-			if (cmp(array[i] != 0)
+			if (cmp(array[i]) != 0)
 				return (i);
 		}
 	}
+	return (-1);
 }
