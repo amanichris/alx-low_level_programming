8f900c3494652b7b76ddf305d0698158277a008f
Chris Sammy <sammy.chrisammy.chris30@gmail.com>
2023-07-16 02:26:20 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,10 @@
 
 /**
  * main - Entry
- *
  * Return: Always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle,");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
