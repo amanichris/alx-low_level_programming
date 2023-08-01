fa2d05ccab4053c1d098d5415cdee89f54b72cf9
Chris Sammy <sammy.chrisammy.chris30@gmail.com>
2023-08-01 04:23:28 -0700

    100-set_string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_string -> Entry point
+ *@s: pointer to pointer
+ *@to: pointer to char
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
+
