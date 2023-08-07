8b50934be39169df2e4105d8258dabe8acfe3b4e
Chris Sammy <sammy.chrisammy.chris30@gmail.com>
2023-08-07 13:05:48 -0700

    0-create_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,38 +4,21 @@
 
 /**
  * create_array - arrays to prints a string
- * @size: number elements array
- * @c: char
- * Return: A pointer
+ * @size: size array
+ * @c: char to assign
+ * Description: create array of size size and assign char c
+ * Return: pointer to array, NULL if fail
  */
 char *create_array(unsigned int size, char c)
 {
-	char *buffer;
-	unsigned int position;
+	char *str;
+	unsigned int i;
 
-	if (size == 0)
-	{
+	str = malloc(sizeof(char) * size);
+	if (size == 0 || str == NULL)
 		return (NULL);
-	}
 
-/* Define value with malloc */
-buffer = (char *) malloc(size * sizeof(c));
-	if (buffer == 0)
-
-	{
-		return (NULL);
-	}
-
-	else
-	{
-		position = 0;
-	while
-		(position < size); /* while for array */
-	{
-	 *(buffer + position) = c;
-		position++;
-	}
-
-	return (buffer);
-	}
+	for (i = 0; i < size; i++)
+		str[i] = c;
+	return (str);
 }
