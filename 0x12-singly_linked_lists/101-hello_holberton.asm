95f1b896887824084011de9773155b12b8837ebc
Chris Sammy <sammy.chrisammy.chris30@gmail.com>
2023-09-26 19:05:23 +0000

    Updated file

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -6,8 +6,6 @@ main:
 	call printf
 	mov eax, 0
 	ret
-<<<<<<< HEAD
+        HEAD
 format: db `Hello, Holberton\n`,0
-=======
-format: db `Hello, Holberton\n`,
->>>>>>> ea2cf201d3967483678bdffc91adc5b98a1503cb
+
