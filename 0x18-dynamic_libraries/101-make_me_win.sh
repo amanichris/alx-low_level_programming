5fd4e32bd72a12e39b0bc0900345f618653b5701
Chris Sammy <sammy.chrisammy.chris30@gmail.com>
2024-01-18 11:20:48 -0800

    hacks resolved

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -p .. https://raw.githubusercontent.com/amanichris/alx-low_level_programming/master/0x18-dynamic_libraries/libhacks.so
+wget -p .. https://raw.githubusercontent.com/amanichris/alx-low_level_
+programming/master/0x18-dynamic_libraries/libhacks.so
 export LD_PRELOAD="$PWD/ ../libhacks.so"
