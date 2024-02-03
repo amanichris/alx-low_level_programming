63bf99704ac58b4beaae877244b54defe76f5b2e
Chris Sammy <sammy.chrisammy.chris30@gmail.com>
2024-02-02 20:59:13 -0800

    6-hash_table_delete

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete -> Deletes a hash table.
+ * @ht: A pointer to a hash table.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_table_t *head = ht;
+	hash_node_t *node, *tmp;
+	unsigned long int index;
+
+	for (index = 0; index < ht->size; index++)
+	{
+		if (ht->array[index] != NULL)
+		{
+			node = ht->array[index];
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
+		}
+	}
+	free(head->array);
+	free(head);
+}
